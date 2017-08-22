1a958f77d9e91d37234556172463299eefadc5a2
Cakarena <30563088+Cakarena@users.noreply.github.com>
2017-08-22 21:06:06 +1000

    Add files via upload

---
+++ jacket.py
@@ -0,0 +1,8 @@
+t=input("Enter the temperture in Celsius: ")
+t=float(t)
+if t < 20 :
+     print("Bring a heavy jacket")
+elif t < 30 :
+    print("Bring a light jacket")
+elif t > 30 :
+	print("Do not bring a jacket")
